97229d1873d21dfc6b0bfe3fae9a6b6f8973a119
laxmankr72 <80440232+laxmankr72@users.noreply.github.com>
2024-03-08 04:34:36 +0000

    datatype

---
+++ 01_Basics/02_datatype.js
@@ -0,0 +1,24 @@
+/*datatypes-
+1. number
+2. string
+3. boolean
+4. undefined
+5. null
+6. symbol
+7. bigint
+*/
+
+let student = "laxman";
+let rollno = 101;
+let alfabet = true;
+let address = null;
+let department ;
+
+console.table(typeof student); //string
+console.table(typeof rollno); //number
+console.table(typeof alfabet); //boolean
+console.table(typeof address); //object
+console.table(typeof department); //undefined
+
+console.table(typeof null);
+console.table(typeof undefined);
