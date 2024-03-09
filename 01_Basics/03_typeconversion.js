62db4233d069e1164531299b2a22e5a6448999d7
Laxman <laxmantelo2003@gmail.com>
2024-03-09 09:31:30 +0530

    conversion in js

---
+++ 01_Basics/03_typeconversion.js
@@ -0,0 +1,23 @@
+let x = 1245;
+
+console.log(typeof x);
+//convert to string
+// String() or toString()
+
+ let z = String(x);
+ let str = x.toString();
+ console.log(typeof z);
+ console.log(typeof str);
+
+//  convert to boolean
+//  Boolean() or !! operator
+
+let bool = Boolean(x);
+console.log(typeof bool);
+console.log(bool);
+
+let str1 = "";
+let constr1 = Boolean(str1);
+console.log(typeof constr1);
+console.log(constr1);
+
