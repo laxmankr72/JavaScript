08fcd9e8ef31cd8e65bbb439d2936706fe05f11e
Laxman <laxmantelo2003@gmail.com>
2024-03-22 00:13:34 +0530

    Explore in array

---
+++ 01_Basics/Array.js
@@ -0,0 +1,39 @@
+const arr = [0,1,24,0,21,45];
+// console.log(arr);
+// console.log(arr.length);
+
+// const myhero = ["papa","mammy"];
+
+// console.log(myhero);
+
+// const myarr = new Array(1,45,25,78);
+// console.log(myarr[2]);
+
+// Array methods
+
+// arr.push(10);
+// console.log(arr);
+// arr.pop();
+// console.log(arr);
+
+// arr.unshift(9);
+// console.log(arr);
+// arr.shift();
+// console.log(arr);
+
+
+// console.log(arr.includes(24));
+// console.log(arr.indexOf(24));
+
+// const myarr2 = arr.join();
+// console.log(arr);
+// console.log(myarr2);
+
+console.log(arr);
+const myarr11 = arr.slice(1,3);
+console.log("a", myarr11);
+console.log(arr);
+
+const myarr12 = arr.splice(1,3);
+console.log("b", myarr12);
+console.log(arr);
