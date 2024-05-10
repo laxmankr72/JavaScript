5bc2d85da9015c2d2723168eb40c7a143f2d6d47
Laxman <laxmantelo2003@gmail.com>
2024-05-10 21:55:13 +0530

    start project, manupulate DOM , and Event handling on button

---
+++ DOM_Projects/Quiz Game App/Script.js
@@ -0,0 +1,42 @@
+const optionObj = {
+    category: `Food & Drink`,
+    id: `qa-1`,
+    correctAnswer: `Three`,
+    options: [`Two`, `Three`, `Four`, `Five`],
+    Question: "How many pieces of bun are in a Mcdonald's Big Mac?",
+};
+
+let Score = 0;
+
+const {correctAnswer,options,Question} = optionObj;
+
+// Accessing all the Element
+const questionEle = document.querySelector('#Question');
+
+const optionEle = document.querySelector('#options');
+
+const scoreEle = document.querySelector('#score');
+
+// Manupulating the DOM
+
+questionEle.textContent = Question;
+
+// populating the Options div with the buttons 
+options.forEach((opt) =>{
+    // creating button and append to the option div
+    const btn = document.createElement('button');
+    btn.textContent = opt;
+    optionEle.appendChild(btn);
+
+    // Event handling on button
+    btn.addEventListener('click', ()=>{
+        if(opt === correctAnswer){
+            Score++;
+        }else{
+            Score = Score-0.25;
+        }
+        scoreEle.textContent = `Score: ${Score}`;
+        questionEle.textContent = "Quiz Completed !!";
+        optionEle.textContent = '';
+    });
+});
